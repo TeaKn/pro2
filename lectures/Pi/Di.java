ca8cae485c300f02342305a89e4e0d70b61126f5
Lovro Šubelj <lovro.subelj@fri.uni-lj.si>
2020-02-17 18:23:07 +0100

    Add material for first Python lectures

---
+++ lectures/Pi/Di.java
@@ -0,0 +1,5 @@
+public class Di {
+  public static void main(String[] args) {
+    System.out.println("Pozdravljeni pri predmetu PRO2!");
+  }
+}
---
+++ lectures/Pi/Di.py
@@ -0,0 +1,1 @@
+print('Pozdravljeni pri predmetu PRO2!')
